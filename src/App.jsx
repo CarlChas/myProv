75d58a85fac9934bf2c273cb5a7a8d24ac7da987
CarlChas <carl.mikaelsson@chasacademy.se>
2024-04-10 21:10:49 +0200

    Reduced to base form

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      {/* Kriterier som måste uppfyllas */}
+      <p>
+        <ul>
+          <li>
+            Olika sätt att använda funktioner - funktionsdeklaration / funktionsuttryck / arrow functions
+          </li>
+          <li>
+            Använda sig av arrayer och objekt
+          </li>
+          <li>
+            Kunna använda callback function
+          </li>
+          <li>
+            Arraymetoder som push, pop, slice, splice, m fl
+          </li>
+          <li>
+            Higher-order-functions map, filter, sort, reduce
+          </li>
+          <li>
+            Använda template strings
+          </li>
+          <li>
+            Använda rest och spread operator
+          </li>
+          <li>
+            Använda desctructering av objekt eller arrayer
+          </li>
+          <li>
+            Kunna hämta från API med fetch
+          </li>
+          <li>
+            Kunna använda Promise-chaining (.then, .catch)
+          </li>
+          <li>
+            Kunna använda async / await
+          </li>
+          <li>
+            Felhantering med try / catch / throw
+          </li>
+          <li>
+            Använda sig av ES6 modules (d.v.s import/export mellan filer)
+          </li>
+          <li>
+            Använda ES6 classes
+          </li>
+        </ul>
       </p>
     </>
   )
